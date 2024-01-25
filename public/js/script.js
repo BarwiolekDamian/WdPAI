8ce911fdd3698e73659b916114be012b127a89a7
Damian <devoxmen@gmail.com>
2024-01-25 15:03:08 +0100

    fix: fixed validation during registration

--- public/js/script.js
+++ public/js/script.js
@@ -1,45 +1,39 @@
-const form = document.querySelector("form");
-const emailInput = form.querySelector('input[name="email"]');
-const confirmedPasswordInput = form.querySelector('input[name="confirmedPassword"]');
-
-function isEmail(email)
+document.addEventListener("DOMContentLoaded", function()
 {
-    return /\S+@\S+\.\S+/.test(email);
-}
+    const form = document.getElementById("registration-form");
+    const emailInput = form.querySelector('input[name="email"]');
+    const passwordInput = form.querySelector('input[name="password"]');
+    const confirmedPasswordInput = form.querySelector('input[name="confirmedPassword"]');
 
-function arePasswordsSame(password, confirmedPassword)
-{
-    return password === confirmedPassword;
-}
+    function isEmail(email)
+    {
+        return /\S+@\S+\.\S+/.test(email);
+    }
 
-function markValidation(element, condition)
-{
-    !condition 
-        ? element.classList.add('no-valid')
-        : element.classList.remove('no-valid');
-}
+    function arePasswordsSame(password, confirmedPassword)
+    {
+        return password === confirmedPassword;
+    }
 
-function validateEmail()
-{
-    setTimeout(function ()
+    function markValidation(element, condition)
+    {
+        !condition 
+            ? element.classList.add('no-valid')
+            : element.classList.remove('no-valid');
+    }
+
+    function validateEmail()
     {
         markValidation(emailInput, isEmail(emailInput.value));
-    }, 1000);
-}
+    }
 
-function validatePassword()
-{
-    setTimeout(function ()
+    function validatePassword()
     {
-            const condition = arePasswordsSame
-            (
-                confirmedPasswordInput.previousElementSibling.value,
-                confirmedPasswordInput.value
-            );
-            
-            markValidation(confirmedPasswordInput, condition);
-        }, 1000);
-}
+        const condition = arePasswordsSame(passwordInput.value, confirmedPasswordInput.value);
+        markValidation(confirmedPasswordInput, condition);
+    }
 
-emailInput.addEventListener('keyup', validateEmail);
-confirmedPasswordInput.addEventListener('keyup', validatePassword);
+    emailInput.addEventListener('input', validateEmail);
+    passwordInput.addEventListener('input', validatePassword);
+    confirmedPasswordInput.addEventListener('input', validatePassword);
+});
--- public/views/register.php
+++ public/views/register.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="/public/css/register.css">
     <title>DialectDo - Registration Page</title>
     <link rel="shortcut icon" type="image/png" href="./public/images/logo.png">
+    <script type="text/javascript" src="/public/js/script.js" defer></script>
 </head>
 
 <body>
@@ -54,54 +55,6 @@
             </div>
         </div>
     </div>
-
-    <script>
-        const form = document.querySelector("form");
-        const emailInput = form.querySelector('input[name="email"]');
-        const confirmedPasswordInput = form.querySelector('input[name="confirmedPassword"]');
-
-        function isEmail(email)
-        {
-            return /\S+@\S+\.\S+/.test(email);
-        }
-
-        function arePasswordsSame(password, confirmedPassword)
-        {
-            return password === confirmedPassword;
-        }
-
-        function markValidation(element, condition)
-        {
-            !condition 
-                ? element.classList.add('no-valid')
-                : element.classList.remove('no-valid');
-        }
-
-        function validateEmail()
-        {
-            setTimeout(function ()
-            {
-                markValidation(emailInput, isEmail(emailInput.value));
-            }, 1000);
-        }
-
-        function validatePassword()
-        {
-            setTimeout(function ()
-            {
-                    const condition = arePasswordsSame
-                    (
-                        confirmedPasswordInput.previousElementSibling.value,
-                        confirmedPasswordInput.value
-                    );
-                    
-                    markValidation(confirmedPasswordInput, condition);
-                }, 1000);
-        }
-
-        emailInput.addEventListener('keyup', validateEmail);
-        confirmedPasswordInput.addEventListener('keyup', validatePassword);
-    </script>
 
 </body>
 
